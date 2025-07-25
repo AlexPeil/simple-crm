77e008755de080f2494d411dafb87e929a7ec4c5
Alexander Peil <alexander.peil@evival.net>
2025-07-25 18:06:05 +0200

    Add CSRF and CORS allowed origins for local development

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,6 +54,22 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+
+  'http://127.0.0.1:5500',
+
+  'http://localhost:5500',
+
+]
+
+CORS_ALLOWED_ORIGINS = [
+
+  'http://127.0.0.1:5500',
+
+  'http://localhost:5500',
+
+]
+
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
